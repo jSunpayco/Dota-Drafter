e1f412d9fec678ce26abae63c7f219a4b2ed22c4
Julian <julian_sunpacyo@dlsu.edu.ph>
2022-11-17 10:52:40 +0800

    Migrate to mongo Atlas
    From local MongoDB instance to Atlas

--- backend/models/about.ts
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-const Schema = mongoose.Schema
-
-const heroes = new Schema({
-    cName: String,
-    cHex: String
-}, {versionKey:false});
-
-module.exports = mongoose.model('heroes', heroes)
