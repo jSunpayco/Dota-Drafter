17819457b5e8bbaaa696f0367ddd257fb12e6d44
Julian <julian_sunpacyo@dlsu.edu.ph>
2022-11-17 11:54:33 +0800

    Disable about

--- backend/routes/hero_status.ts
+++ backend/routes/hero_status.ts
@@ -1,13 +1,7 @@
 const {Router} = require('express')
 import express from "express";
 const router = express.Router();
-// const anItem = require('../models/hero_status.ts');
 const dbo = require('../dbConn');
-
-// router.get('/', (req:any, res:any) => {
-//     anItem.find()
-//         .then((items:any) => res.json(items))
-// });
 
 router.route('/heroStatus').get(async function (_req:any, res:any) {
     const dbConnect = dbo.getDb();
@@ -15,7 +9,7 @@ router.route('/heroStatus').get(async function (_req:any, res:any) {
     dbConnect
       .collection('heroes')
       .find({})
-      .limit(50)
+      // .limit(50)
       .toArray(function (err:any, result:any) {
         if (err) {
           res.status(400).send('Error fetching listings!');
