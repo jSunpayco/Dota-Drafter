7752f209de1c981c9080681755bea4a114ff71dd
Julian <julian_sunpacyo@dlsu.edu.ph>
2022-12-12 12:37:53 +0800

    Footer

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Heroes from './pages/Heroes/Heroes.tsx';
 import Drafter from './pages/Drafter.tsx';
 import Home from './pages/Home/Home.tsx';
 import About from './pages/About.tsx';
+import Footer from './components/Footer.tsx';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 export default function App() {
@@ -15,6 +16,7 @@ export default function App() {
         <Route path="/about" element={<About />} />
         
       </Routes>
+      <Footer/>
     </Router>
   );
 }
---
+++ src/components/Footer.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+
+function Footer() {
+
+  return (
+    <footer style={{ left: 0, width: '100%', height: '48px', position:'absolute', backgroundColor: '#228BE6', display: 'flex', alignItems: 'center', justifyContent:'center', color:'white' }}>
+      This is a personal project and is in no way affiliated with DOTA 2, Valve and all its assets.
+    </footer>
+  );
+};
+
+export default Footer;
