5c98aa91e1bc55dbef7fcea5074fc799c4a3d90a
Julian <julian_sunpacyo@dlsu.edu.ph>
2022-12-15 09:47:45 +0800

    Fixed footer

--- src/App.tsx
+++ src/App.tsx
@@ -4,18 +4,20 @@ import Drafter from './pages/Drafter.tsx';
 import Home from './pages/Home/Home.tsx';
 import About from './pages/About.tsx';
 import Footer from './components/Footer.tsx';
+import './styles.css'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 export default function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/drafter" element={<Drafter />} />
-        <Route path="/heroes" element={<Heroes />} />
-        <Route path="/about" element={<About />} />
-        
-      </Routes>
+      <div id='main'>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/drafter" element={<Drafter />} />
+          <Route path="/heroes" element={<Heroes />} />
+          <Route path="/about" element={<About />} />
+        </Routes> 
+      </div>
       <Footer/>
     </Router>
   );
